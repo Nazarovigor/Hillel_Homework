0ee92aa3b1c0571741bfc69e20635e77f8c87c29
unknown <nazarov.igor.anatolievich@gmal.com>
2023-06-26 22:00:34 +0300

    Selenium UI Test

--- Lesson13/test/my_test.py
+++ Lesson13/test/my_test.py
@@ -1,10 +1,12 @@
 import pytest
-from analizer import SourceAnalyzer
+from .analizer import SourceAnalyzer
+
 
 def test_get_links_from_url(source_analyzer):
     url = "https://google.com"
     links = source_analyzer.get_links_from_url(url)
     assert len(links) > 0
+
 
 def test_get_links_from_pdf(source_analyzer):
     pdf_path = r"D:\1.pdf"
